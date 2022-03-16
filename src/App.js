93b00c6d83e98fad05f419396deeacaf004fbdcb
7sering <codenow8@gmail.com>
2022-03-16 17:02:58 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Featured from "./components/featured/Featured";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from './components/navbar/Navbar';
+import Signup from "./components/signup/Signup";
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Featured/>
+    <Signup/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
